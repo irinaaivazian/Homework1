66cf1c6e89491a2a141d0d6927aa9a32c73c60b1
Irina <Irina@LAPTOP-P147OHQP>
2024-05-31 22:48:55 +0400

    Add project files.

---
+++ Project1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Integer types
+            byte byteValue = 255;
+            sbyte sbyteValue = -128;
+            short shortValue = 32767;
+            ushort ushortValue = 65535;
+            int intValue = 2147483647;
+            uint uintValue = 4294967295;
+            long longValue = 9223372036854775807;
+            ulong ulongValue = 18446744073709551615;
+
+            // Floating-point types
+            float floatValue = 3.14f;
+            double doubleValue = 3.141592653589793;
+            decimal decimalValue = 79228162514264337593543950335m;
+
+            // Character type
+            char charValue = 'A';
+
+            // Boolean type
+            bool boolValue = true;
+
+            // String type
+            string stringValue = "Hello, world!";
+
+            // Object type (can hold any data type)
+            object objectValue = 123;
+        }
+    }
+}
